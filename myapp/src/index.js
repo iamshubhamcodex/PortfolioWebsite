f70a19c6fe3d9fb5722452ab386a64ba0f4e83fd
Shubham Singh <shubhamsinghcodex@gmail.com>
2022-09-09 00:04:06 +0530

    Modified

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const loc = window.location.href.split("?");
+
+const desk = () => {
+  return <App />;
+};
+const mob = () => {
+  const loc1 = loc[1].split("=")[1];
+  console.log(loc1);
+  if (loc1 === "none") return <App />;
+};
+const handl = () => {
+  if (loc.length === 1) return desk();
+  else if (loc.length > 1) return mob();
+};
+
+root.render(<React.StrictMode>{handl()}</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
